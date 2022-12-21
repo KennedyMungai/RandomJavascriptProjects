9936ed7a257d41b726ec33ff4849f05f86369724
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 12:05:25 +0300

    My dumb solution

--- Counter/app.js
+++ Counter/app.js
@@ -2,3 +2,33 @@ let count = 0
 
 const btns = document.querySelectorAll('.btn')
 const value = document.querySelector('#value')
+
+btns.forEach(function (btn)
+{
+    if (btn.classList.contains('increase'))
+    {
+        btn.addEventListener('click', function ()
+        {
+            count++
+        })
+
+    }
+    else if (btn.classList.contains('decrease'))
+    {
+        btn.addEventListener('click', function ()
+        {
+            count--
+        })
+    }
+    else if (btn.classList.contains('reset'))
+    {
+        btn.addEventListener('click', function ()
+        {
+            count = 0
+        })
+    }
+    else
+    {
+        return;
+    }
+})
