beb1cbc1d0fa2199a5f00a12209bf1bf97a65c62
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 15:02:56 +0300

    Added some logic that will allow us to cycle through the profiles

--- Reviews/app.js
+++ Reviews/app.js
@@ -68,6 +68,12 @@ function showPerson(person)
 nextBtn.addEventListener('click', function ()
 {
   currentItem++
+
+  if (currentItem > reviews.length - 1)
+  {
+    currentItem = 0
+  }
+
   showPerson(currentItem)
 })
 
