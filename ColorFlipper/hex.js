63c104441267b9e62064d54d641cd5b68754f477
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 11:50:13 +0300

    Made a few changes that made my background respond to the randomly generated color stings

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -1,18 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn')
-const color = document.getElementsByClassName('color')
+const color = document.querySelector('.color')
 
 btn.addEventListener('click', function ()
 {
-    let hexColor = '#'
+    let hexColor = "#"
 
     for (i = 0; i < 6; i++)
     {
-        let item = Math.floor(Math.random * hex.length)
+        let item = Math.floor(Math.random() * hex.length)
         hexColor += hex[item]
     }
 
     document.body.style.backgroundColor = hexColor
-    color.textContent = hexColor
+    color.innerHTML = hexColor
 })
