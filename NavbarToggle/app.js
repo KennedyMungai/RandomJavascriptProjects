e23ead561ae04d9c719676b0750e379272705ff3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 20:13:35 +0300

    Refactored the code

--- NavbarToggle/app.js
+++ NavbarToggle/app.js
@@ -9,12 +9,14 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function ()
 {
-    if (links.classList.contains('show-links'))
-    {
-        links.classList.remove('show-links')
-    }
-    else
-    {
-        links.classList.add('show-links')
-    }
+    // if (links.classList.contains('show-links'))
+    // {
+    //     links.classList.remove('show-links')
+    // }
+    // else
+    // {
+    //     links.classList.add('show-links')
+    // }
+
+    links.classList.toggle('show-links')
 });
