be3beaa09b67439cc5f0ec16271e603ff5c3f127
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 20:54:19 +0300

    Added a click event listener to the toggleBtn

--- SideBarToggle/app.js
+++ SideBarToggle/app.js
@@ -1,3 +1,8 @@
-const toggleButton = document.querySelector('.sidebar-toggle')
+const toggleBtn = document.querySelector('.sidebar-toggle')
 const sidebar = document.querySelector('.sidebar')
 const closeBtn = document.querySelector('close-btn')
+
+toggleBtn.addEventListener('click', function ()
+{
+    console.log(sidebar.classList)
+})
