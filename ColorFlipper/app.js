8bbcabf5d7a13ed12439b37dde0ccd2e2ca4240e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 11:33:05 +0300

    My dumb ass solution

--- ColorFlipper/app.js
+++ ColorFlipper/app.js
@@ -2,3 +2,10 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn')
 const color = document.querySelector('.color')
+const page = document.querySelector('.container')
+
+btn.addEventListener('click', function ()
+{
+    colorIndex = Math.floor(Math.random() * colors.length - 1)
+    main.style.backgroundColor = colors[colorIndex]
+})
