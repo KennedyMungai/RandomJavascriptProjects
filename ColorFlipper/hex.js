766b8773faffa3c220a2ad322e3b543d0a1b02b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-21 11:47:57 +0300

    Another one of my questionable solutions

--- ColorFlipper/hex.js
+++ ColorFlipper/hex.js
@@ -2,3 +2,17 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById('btn')
 const color = document.getElementsByClassName('color')
+
+btn.addEventListener('click', function ()
+{
+    let hexColor = '#'
+
+    for (i = 0; i < 6; i++)
+    {
+        let item = Math.floor(Math.random * hex.length)
+        hexColor += hex[item]
+    }
+
+    document.body.style.backgroundColor = hexColor
+    color.textContent = hexColor
+})
