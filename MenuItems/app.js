da15320509535a83e2a05fa2a136942a9bdafdf7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-27 14:43:37 +0300

    Console logged out the dataset id

--- MenuItems/app.js
+++ MenuItems/app.js
@@ -88,7 +88,7 @@ filterBtns.forEach(function (btn)
 {
   btn.addEventListener('click', function (e)
   {
-    console.log(e.currentTarget.dataset)
+    console.log(e.currentTarget.dataset.id)
   })
 })
 
