ef3457bd3777ae51c5a08b34208270abde86180a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-27 10:38:10 +0300

    Wrote the one accordion open per button click functionality

--- Questions/app.js
+++ Questions/app.js
@@ -20,6 +20,15 @@ questions.forEach(function (question)
     // console.log(btn)
     btn.addEventListener('click', function ()
     {
+        questions.forEach(function (item)
+        {
+            // console.log(item)
+            if (item !== question)
+            {
+                item.classList.remove('show-text')
+            }
+        })
+
         question.classList.toggle('show-text')
     })
 })  
