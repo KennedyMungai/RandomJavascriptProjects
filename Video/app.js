1dc45a7681aef5a4f3003c81af0693f7abf778ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-28 12:42:24 +0300

    Hid the preloader after the content is loaded

--- Video/app.js
+++ Video/app.js
@@ -3,6 +3,7 @@
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 const btn = document.querySelector('.switch-btn')
 const video = document.querySelector('.video-container')
+const preloader = document.querySelector('.preloader')
 
 btn, addEventListener('click', function ()
 {
@@ -16,4 +17,9 @@ btn, addEventListener('click', function ()
         btn.classList.remove('slide')
         video.play()
     }
+})
+
+window.addEventListener('load', function ()
+{
+    preloader.classList.add('hide-preloader')
 })
