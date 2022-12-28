60cfb5c821c935a48a3e250b746f501ea39f45c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-28 12:32:59 +0300

    Added the play pause behaviour

--- Video/app.js
+++ Video/app.js
@@ -9,9 +9,11 @@ btn, addEventListener('click', function ()
     if (!btn.classList.contains('slide'))
     {
         btn.classList.add('slide')
+        video.pause()
     }
     else
     {
         btn.classList.remove('slide')
+        video.play()
     }
 })
