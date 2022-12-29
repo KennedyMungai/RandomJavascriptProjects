a36d102225e6d4b151fabc942664a80494c42090
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-29 11:02:21 +0300

    Added logic to remove the active css class

--- Tabs/app.js
+++ Tabs/app.js
@@ -5,5 +5,15 @@ const articles = document.querySelectorAll('.content')
 
 about.addEventListener('click', function (e)
 {
-    console.log(e.target.dataset.id)
+    // console.log(e.target.dataset.id)
+    const id = e.target.dataset.id
+
+    if (id)
+    {
+        // remove the active class from other buttons
+        btns.forEach(function (btn)
+        {
+            btn.classList.remove('active')
+        })
+    }
 })
