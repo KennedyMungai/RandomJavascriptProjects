6747c3b0691c0aa3d68507f347ea35d3e79332c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-30 19:42:25 +0300

    Uncommented the code to remove all the items from the local storage

--- GroceryBud/app.js
+++ GroceryBud/app.js
@@ -118,7 +118,7 @@ function clearItems()
     container.classList.remove('show-container')
     displayAlert("empty list", "danger")
     setBackToDefault()
-    // localStorage.removeItem('list')
+    localStorage.removeItem('list')
 }
 
 // edit function
