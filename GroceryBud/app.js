281b9a23a47329919da2e27e1f28d886bf157b2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-30 14:23:17 +0300

    Added references to more items on the DOM

--- GroceryBud/app.js
+++ GroceryBud/app.js
@@ -3,6 +3,9 @@ const alert = document.querySelector('.alert')
 const form = document.querySelector('.grocery-form')
 const grocery = document.getElementById('grocery')
 const submitBtn = document.querySelector('.submit-btn')
+const container = document.querySelector('.grocery-container')
+const list = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
 // edit option
 
 // ****** EVENT LISTENERS **********
