04fd50f6d083bd45b5e1e9e0298e148a2571e2a1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-30 20:37:55 +0300

    Moved items to the left based on their index

--- Slider/app.js
+++ Slider/app.js
@@ -1,3 +1,8 @@
 const slides = document.querySelectorAll('.slide')
 const nextBtn = document.querySelector('.nextBtn')
 const prevBtn = document.querySelector('.prevBtn')
+
+slides.forEach(function (slide, index)
+{
+    slide.style.left = `${index * 100}%`
+})
