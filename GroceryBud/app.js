6b028fe14101fa50ce8443f2dcddfad0015fe40f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-12-30 19:31:57 +0300

    Moved the getLocalStorage code to its own line

--- GroceryBud/app.js
+++ GroceryBud/app.js
@@ -159,7 +159,8 @@ function deleteItem(e)
 function addToLocalStorage(id, value)
 {
     const grocery = { id, value }
-    let items = localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : []
+    let items =
+        console.log(items)
     items.push(grocery)
     localStorage.setItem('list', JSON.stringify(items))
 }
@@ -172,5 +173,10 @@ function removeFromLocalStorage(id)
 function editLocalStorage(id, value)
 {
     // console.log()
+}
+
+function getLocalStorage()
+{
+    return localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : []
 }
 // ****** SETUP ITEMS **********
